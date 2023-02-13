9afcbb3cd4653fe23cf0f13fad43409288e3af1c
kennedy <kenneyrit@gmail.com>
2023-02-13 12:31:20 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+#defining a class Anagram
+class Anagram
+  #initializing the class
+  def initialize(anagram)
+      @anagram = anagram
+  end
+
+  #iteratring through the class
+  def match(words)
+      words.select {|word| is_inagram?(word)}
+  end
+
+      def is_inagram?(word)
+          word.chars.sort == @anagram.chars.sort
+      end
+
+  end 
